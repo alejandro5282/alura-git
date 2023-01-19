29666ad015b8fafeb59b9368a335fc3cd09142c7
alejandro <edaleor@hotmail.com>
2023-01-19 00:30:47 -0500

    creando archivo index.hmtl con lista de curso

---
+++ edwin.js
@@ -0,0 +1,26 @@
+var minombre = "alejandro"
+console.log(minombre)
+console.log(window)
+
+function mostrarminombre(){
+
+
+    console.log(minombre)
+
+}
+
+mostrarminombre()
+
+function mostrarmiapellido(){
+
+ var mostrarmiapellido = "ortiz"
+ console.log(mostrarmiapellido)
+
+}
+mostrarmiapellido()
+
+if(true){
+
+let animal = "gato"
+
+}
